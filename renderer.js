283b0f368fe17d04dcf7d0b3c4790bcb81125481
srithwak <srithwak@gmail.com>
2024-03-21 12:37:21 -0400

    change to test folder

--- renderer.js
+++ renderer.js
@@ -1,31 +1,28 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
 
 function createWindow() {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  });
+   const mainWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+         nodeIntegration: true
+      }
+   });
 
-  mainWindow.loadURL('http://localhost:1234'); // Load content from port 50794
-
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
+   mainWindow.loadFile(path.join(__dirname, 'app.html'));
 }
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+   if (process.platform !== 'darwin') {
+      app.quit();
+   }
 });
 
 app.on('activate', function () {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+   if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+   }
 });
